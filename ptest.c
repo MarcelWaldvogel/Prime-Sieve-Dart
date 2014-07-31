1afffc7f756c3a286f67b7da68da17add71e2275
hacatu <hacatu5000@gmail.com>
2014-07-30 20:56:52 -0400

    Update ptest.c

--- ptest.c
+++ ptest.c
@@ -11,7 +11,7 @@ int main(int argc, char** argv){
 	uint64_t max=1000000;
 	char print=1;
 	if(argc>1){
-		max=uint64_t(atoll(argv[1]));
+		max=atoll(argv[1]);
 	}
 	if(argc>2){
 		print=atoi(argv[2]);
