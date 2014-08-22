a036b5e492ec45b2e7ef90944876b55a473afafd
hacatu <hacatu5000@gmail.com>
2014-08-22 12:03:50 -0400

    fixed isPrime being named is_prime and made isPrime take a const pointer

--- sieve210.h
+++ sieve210.h
@@ -8,4 +8,4 @@
 uint64_t* sievePrimes(uint64_t max);
 
 //checks if a number is prime by binary searching an array of primes for it.
-char is_prime(uint64_t n, uint64_t* primes);
+char isPrime(uint64_t n, const uint64_t* primes);
